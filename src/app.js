17fd072bc2c13ad473566de07b039f8654d5b7ac
Stephen Moon <stephenmoon@college.harvard.edu>
2022-07-13 12:48:55 -0400

    Checking task 2

--- src/app.js
+++ src/app.js
@@ -12,18 +12,18 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(pathToStatic));
 
-const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync('src/json/users.json', 'utf8');
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 const users = JSON.parse(userData);
 
 app.get('/', (req, res) => res.render('index', {title: 'Account Summary', accounts : account}));
 
 app.get('/savings', (req, res) => res.render('account', {account : accounts.savings}));
-app.get('/savings', (req, res) => res.render('account', {account : accounts.checking}));
-app.get('/savings', (req, res) => res.render('account', {account : accounts.credit}));
+app.get('/checking', (req, res) => res.render('account', {account : accounts.checking}));
+app.get('/credit', (req, res) => res.render('account', {account : accounts.credit}));
 
 app.get('/profile', (req, res) => res.render('profile', {user: users[0]}));
 
-app.listen(3000, () => console.log('PS Project Running on port 3000!'));
+app.listen(3000, () => { console.log('PS Project Running on port 3000!')});
